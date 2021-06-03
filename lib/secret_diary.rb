0e7dc79d099286e89e23faeeede3d8427d204392
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-06-03 11:22:20 +0100

    Added exceptions to add_entry and get_entries methods

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,18 +1,27 @@
 class SecretDiary
+  attr_reader :locked
+
+  def initialize
+    @locked = true
+  end 
+
   def lock
-    true
-    'Locked'
+    @locked = true
+   'Locked'
   end
 
   def unlock
+    @locked = false
     'Unlocked'
   end
 
   def add_entry
+    fail 'Locked, no entry!' if @locked == true
     'Begin entry!'
   end
 
   def get_entries
+    fail 'Locked, cannot display entries!' if @locked == true
     'Showing entries:'
   end
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -12,20 +12,36 @@
 require 'secret_diary.rb'
 
 describe SecretDiary do
+
   it 'SecretDiary is locked' do
      expect(subject.lock).to eq('Locked')
   end
+  
 
   it 'SecretDiary is unlocked' do
       expect(subject.unlock).to eq('Unlocked')
   end
 
-  it 'Add entry' do
-    expect(subject.add_entry).to eq('Begin entry!')
+  describe '#add_entry' do
+    it 'Add entry' do
+      subject.unlock
+      expect(subject.add_entry).to eq('Begin entry!')
+    end
+
+    it 'raises error when locked' do
+      expect{ subject.add_entry }.to raise_error 'Locked, no entry!'
+    end
   end
 
-  it 'Get entries' do
-    expect(subject.get_entries).to eq('Showing entries:')
+  describe '#get_entries' do
+    it 'Get entries' do
+      subject.unlock
+      expect(subject.get_entries).to eq('Showing entries:')
+    end
+
+    it 'raises error when locked' do
+      expect{ subject.get_entries }.to raise_error 'Locked, cannot display entries!'
+    end
   end
 
 end
